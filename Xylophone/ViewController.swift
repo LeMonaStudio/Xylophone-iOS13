0850ee8c14080593a1ec3b28de6777c6cecba091
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-02-27 09:05:11 +0100

    Completed the Xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let seconds = 0.2
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            sender.alpha = 1
+        }
+        
+        playSound(resourceName: sender.currentTitle!)
+    }
+    
+    func playSound(resourceName: String) {
+        guard let url = Bundle.main.url(forResource: resourceName, withExtension: "wav") else {return}
+        do {
+            player = try! AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+        
+    }
 }
 
